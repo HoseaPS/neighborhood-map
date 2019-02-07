3be042b7b9dab9024dc80560da0bb5326f1c5662
Oséias P. Silva <hoseaps@gmail.com>
2019-02-07 07:18:03 -0200

    credentials update

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faBars } from "@fortawesome/free-solid-svg-icons";
+
+class NavBar extends Component {
+  state = { isActive: false };
+
+  toogleButton = () => {
+    this.setState({ isActive: !this.state.isActive });
+  };
+
+  render() {
+    const { OnToogleSidebar } = this.props;
+
+    return (
+      <div className="navbar">
+        <button
+          className="sidebarLink"
+          onClick={() => this.toogleButton() || OnToogleSidebar()}
+        >
+          <FontAwesomeIcon icon={this.state.isActive ? faArrowLeft : faBars} />
+        </button>
+        <div className="title">Hamburguerias do Bairro</div>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
---
+++ src/pages/main/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import NavBar from "./../../components/NavBar/index";
+import SideBar from "./../../components/SideBar/index";
+import MapComponent from "./../../components/Map/index";
+import * as FoursquareAPI from "./../../services/foursquareApi";
+import escapeRegExp from "escape-string-regexp";
+import "./../../styles/global";
+
+const lat = -22.3569919;
+const lng = -47.3990678;
+
+class Main extends Component {
+  state = {
+    sidebarActive: false,
+    locations: [],
+    query: ""
+  };
+
+  componentDidMount() {
+    FoursquareAPI.getVenues(lat, lng).then(venues => {
+      if (!venues) return;
+      const locations = venues.map(venue => {
+        return {
+          id: venue.id,
+          name: venue.name,
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          formattedAddress: venue.location.formattedAddress[0],
+          isMarkerShown: false
+        };
+      });
+
+      this.setState({ locations: locations });
+    });
+  }
+
+  toogleSidebar = () => {
+    this.setState({ sidebarActive: !this.state.sidebarActive });
+  };
+
+  filterLocations = query => {
+    const match = new RegExp(escapeRegExp(query), "i");
+    return this.state.locations.filter(loc => match.test(loc.name));
+  };
+
+  updateQuery = query => {
+    this.setState({ query: query });
+  };
+
+  toogleMarkerLocation = location => {
+    location.isMarkerShown = !location.isMarkerShown;
+    this.setState({ locations: this.state.locations });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Route
+          path="/"
+          render={() => (
+            <div className="app-content">
+              <SideBar
+                SidebarActive={this.state.sidebarActive}
+                OnChangeText={this.updateQuery}
+                OnClickText={this.toogleMarkerLocation}
+                query={this.state.query}
+                locations={this.filterLocations(this.state.query)}
+              />
+              <div className="content">
+                <NavBar OnToogleSidebar={this.toogleSidebar} />
+                <MapComponent
+                  lat={lat}
+                  lng={lng}
+                  locations={this.filterLocations(this.state.query)}
+                  OnMarkerClick={this.toogleMarkerLocation}
+                />
+              </div>
+            </div>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default Main;
